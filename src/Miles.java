16fb555725a02ea5f38b6193d9ca73a9a36446aa
Petr Filatov <pmfilatov@gmail.com>
2020-09-15 09:12:22 +0300

    Initial commit

---
+++ src/Miles.java
@@ -0,0 +1,8 @@
+public class Miles {
+    public static void main(String[] args){
+        int priceTicket = 34560;
+        int bonusMiles = priceTicket / 20;
+
+        System.out.println(bonusMiles);
+    }
+}
